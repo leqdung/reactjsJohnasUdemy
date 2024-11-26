9ba4c70753406596d5de8b92b759db2f278e7579
leqdung <dzung.dg@gmail.com>
2024-11-26 13:36:25 +0700

    add comp Loader and MovieList

--- src/App.js
+++ src/App.js
@@ -120,7 +120,16 @@ export default function App() {
     </>
   );
 }
-
+function Loader() {
+  return <p className='loader'>Loading...</p>;
+}
+function ErrorMessage({ message }) {
+  return (
+    <p className='error'>
+      <span>⛔️</span> {message}
+    </p>
+  );
+}
 function NavBar({ children }) {
   return (
     <nav className='nav-bar'>
